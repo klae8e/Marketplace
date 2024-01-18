bbdd9b883cbb91b04318328a05ca923b83e8c270
klae8e <temok031@gmail.com>
2024-01-18 23:07:02 +0500

    added change password in profile

--- main/urls.py
+++ main/urls.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView
 from django.urls import path
 
 from . import views
@@ -14,4 +14,6 @@ urlpatterns = [
     path('accounts/login/', LoginView.as_view(), name='login', kwargs={'redirect_authenticated_user': True}),
     path('accounts/logout/', LogoutView.as_view(next_page='main:profile'), name='logout'),
     path('register', RegisterView.as_view(), name='register'),
+    path('password_change/', PasswordChangeView.as_view(), name='password_change'),
+    path('password_change_done/', PasswordChangeDoneView.as_view(), name='password_change_done'),
 ]
